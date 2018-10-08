d176fcf1bc3e043fec310f6785da4d8918041c39
hatobus <s1240056@gmail.com>
2018-10-08 23:11:36 +0900

    attach licence

--- README.md
+++ README.md
@@ -10,5 +10,30 @@ This is alexa skill for [Smartagri](https://github.com/hatobus/Smartagri)
 - Amazon Alexa
 - Golang
 
+## Work flow 
 
+![workflow](./pic/Smartagri.jpg)
 
+1. Amazon Alexa request for AWS lambda.
+2. To Get request and get information from VPS server.
+3. VPS return JSON.
+4. To perse JSON attach to request.
+5. lambda return json for Alexa
+6. Alexa speaking.
+
+## exsample (lang:ja)
+
+**To request full information **
+
+「アレクサ、{machineNO}の情報を教えて」
+
+「{machineNO}の情報は、温度は15.71度、湿度は71.97パーセント、水分量は0.00パーセント、二酸化炭素濃度は190.00ppm、照度は9539.00ルクスです。この情報は{time}に取得された情報です。」
+
+## Copyright
+
+If you want to information of this repository licence.
+
+You'll see ./LICENSE
+
+Guard object of logger for debugging.
+Copyright (c) 2018 Haga Fumito <s1240056@gmail.com>
--- smartagri.go
+++ smartagri.go
@@ -1,3 +1,19 @@
+/*
+  Copyright [2018] [Haga Fumito]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package main
 
 import (
@@ -183,7 +199,7 @@ func getFarmInfoFromAPI(machineNO string) (AgriData, error) {
 	var resdata []AgriData
 	var tmpdata AgriData
 
-	res, err := http.Get(URL)
+	res, err := http.Get(APIURL)
 	if err != nil {
 		log.Println("API呼び出せてません！！！！")
 		return tmpdata, err
