791fdda9cbae963b5bd72cb2acc85cebc91f60cc
hatobus <s1240056@gmail.com>
2019-10-05 10:56:50 +0900

    remove struct and environment file

--- smartagri.go
+++ smartagri.go
@@ -23,31 +23,17 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	alexa "github.com/ericdaugherty/alexa-skills-kit-golang"
 	"github.com/hatobus/UKEMOCHI/agrialexa"
-	"github.com/joho/godotenv"
 )
-
-var err = godotenv.Load()
-var URL = os.Getenv("APIURL")
-var AppID = os.Getenv("APPID")
-
-var ALX = &alexa.Alexa{ApplicationID: AppID, RequestHandler: &agrialexa.SmartAgri{}, IgnoreApplicationID: true, IgnoreTimestamp: true}
-
-type SmartAgri struct{}
-
-type AgriData struct {
-	No               string `json:"no"`
-	Date             string `json:"date"`
-	Time             string `json:"time"`
-	Temperature      string `json:"temperature"`
-	Humidity         string `json:"humidity"`
-	SoilHumidity     string `json:"soil_humidity"`
-	Co2Concentration string `json:"co2_concentration"`
-	Wavelength       string `json:"wavelength"`
-	Illuminance      string `json:"illuminance"`
-}
 
 // Handle processes calls from Lambda
 func Handle(ctx context.Context, requestEnv *alexa.RequestEnvelope) (interface{}, error) {
+	ALX := &alexa.Alexa{
+		ApplicationID:       os.Getenv("APPID"),
+		RequestHandler:      &agrialexa.SmartAgri{},
+		IgnoreApplicationID: true,
+		IgnoreTimestamp:     true,
+	}
+
 	return ALX.ProcessRequest(ctx, requestEnv)
 }
 
